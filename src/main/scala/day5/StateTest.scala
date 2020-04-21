e7c6e755a39059935a385238f4ff06eb3c864407
wushengran <shengran.wu@163.com>
2020-04-21 15:55:22 +0800

    Day 5 : state programming test

--- src/main/scala/day5/StateTest.scala
+++ src/main/scala/day5/StateTest.scala
@@ -3,9 +3,13 @@ package day5
 import java.util
 
 import com.atguigu.apitest.SensorReading
-import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.common.functions.{MapFunction, ReduceFunction, RichFlatMapFunction, RichMapFunction}
 import org.apache.flink.api.common.state._
 import org.apache.flink.configuration.Configuration
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
+import org.apache.flink.runtime.state.filesystem.FsStateBackend
+import org.apache.flink.runtime.state.memory.MemoryStateBackend
+import org.apache.flink.streaming.api.checkpoint.ListCheckpointed
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.util.Collector
@@ -24,6 +28,11 @@ object StateTest {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
+    // 配置状态后端
+//    env.setStateBackend( new MemoryStateBackend() )
+//    env.setStateBackend( new FsStateBackend("") )
+//    env.setStateBackend( new RocksDBStateBackend("", true) )
+
     val inputStream = env.socketTextStream("localhost", 7777)
     val dataStream: DataStream[SensorReading] = inputStream
       .map( data => {
@@ -31,10 +40,69 @@ object StateTest {
         SensorReading( dataArray(0), dataArray(1).toLong, dataArray(2).toDouble )
       } )
 
+    val warningStream: DataStream[(String, Double, Double)] = dataStream
+      .keyBy("id")
+//      .flatMap( new TempChangeWarningWithFlatmap(10.0) )
+      .flatMapWithState[(String, Double, Double), Double]({
+      case (inputData: SensorReading, None) => (List.empty, Some(inputData.temperature))
+      case (inputData: SensorReading, lastTemp: Some[Double]) => {
+        val diff = (inputData.temperature - lastTemp.get).abs
+        if( diff > 10.0 ){
+          ( List( (inputData.id, lastTemp.get, inputData.temperature) ), Some(inputData.temperature) )
+        } else {
+          (List.empty, Some(inputData.temperature))
+        }
+      }
+    })
+
+    warningStream.print()
+
     env.execute("state test job")
   }
 }
 
+// 自定义 RichMapFunction
+class TempChangeWarning(threshold: Double) extends RichMapFunction[SensorReading, (String, Double, Double)]{
+  // 定义状态变量，上一次的温度值
+  private var lastTempState: ValueState[Double] = _
+
+  override def open(parameters: Configuration): Unit = {
+    lastTempState = getRuntimeContext.getState(new ValueStateDescriptor[Double]("last-temp", classOf[Double]))
+  }
+
+  override def map(value: SensorReading): (String, Double, Double) = {
+    // 从状态中取出上次的温度值
+    val lastTemp = lastTempState.value()
+    // 更新状态
+    lastTempState.update(value.temperature)
+
+    // 跟当前温度值计算差值，然后跟阈值比较，如果大于就报警
+    val diff = (value.temperature - lastTemp).abs
+    if( diff > threshold ){
+      ( value.id, lastTemp, value.temperature )
+    } else
+      ( value.id, 0.0, 0.0 )
+  }
+}
+
+// 自定义 RichFlatMapFunction，可以输出多个结果
+class TempChangeWarningWithFlatmap(threshold: Double) extends RichFlatMapFunction[SensorReading, (String, Double, Double)]{
+  lazy val lastTempState: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("last-temp", classOf[Double]))
+  override def flatMap(value: SensorReading, out: Collector[(String, Double, Double)]): Unit = {
+    // 从状态中取出上次的温度值
+    val lastTemp = lastTempState.value()
+    // 更新状态
+    lastTempState.update(value.temperature)
+
+    // 跟当前温度值计算差值，然后跟阈值比较，如果大于就报警
+    val diff = (value.temperature - lastTemp).abs
+    if( diff > threshold ){
+      out.collect( (value.id, lastTemp, value.temperature) )
+    }
+  }
+}
+
+// keyed state定义示例
 class MyProcessor extends KeyedProcessFunction[String, SensorReading, Int]{
 //  lazy val myState: ValueState[Int] = getRuntimeContext.getState( new ValueStateDescriptor[Int]("my-state", classOf[Int]) )
   lazy val myListState: ListState[String] = getRuntimeContext.getListState(new ListStateDescriptor[String]("my-liststate", classOf[String]))
